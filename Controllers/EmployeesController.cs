36f85615831807c73ca3d7c5fc6d0aed1d4a9760
raj0987-alt <71288207+raj0987-alt@users.noreply.github.com>
2023-06-17 00:06:21 +0600

    static get

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,28 @@
+﻿using ASP.NETCore6WebAPICRUDWithEntityFramework_CodeFirstApproach_.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NETCore6WebAPICRUDWithEntityFramework_CodeFirstApproach_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> Get()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee
+                {
+                    Id = 1,
+                    Name = "Jon Doe",
+                    FirstName = "Jon",
+                    LastName = "Doe",
+                    Email = "john_doe@email.com"
+                }
+            };
+            return Ok(employees);
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace ASP.NETCore6WebAPICRUDWithEntityFramework_CodeFirstApproach_.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
