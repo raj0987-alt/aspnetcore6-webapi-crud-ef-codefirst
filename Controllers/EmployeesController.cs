567eb964bc40112c5fb4f30f1b6e2628d621295b
raj0987-alt <71288207+raj0987-alt@users.noreply.github.com>
2023-06-17 09:18:16 +0600

    post static

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -8,20 +8,49 @@ namespace ASP.NETCore6WebAPICRUDWithEntityFramework_CodeFirstApproach_.Controlle
     [ApiController]
     public class EmployeesController : ControllerBase
     {
-        [HttpGet]
-        public async Task<ActionResult<List<Employee>>> Get()
-        {
-            var employees = new List<Employee>
+        private static List<Employee> employees = new List<Employee>
             {
-                new Employee
-                {
+                new Employee{
                     Id = 1,
                     Name = "Jon Doe",
                     FirstName = "Jon",
                     LastName = "Doe",
-                    Email = "john_doe@email.com"
-                }
+                    Email = "jon_doe@email.com"
+                },
+                new Employee{
+                    Id = 2,
+                    Name = "Will Smith",
+                    FirstName = "Will",
+                    LastName = "Smith",
+                    Email = "will_smith@email.com"
+                },
+                new Employee{
+                    Id = 3,
+                    Name = "Allen Shar",
+                    FirstName = "Allen",
+                    LastName = "Shar",
+                    Email = "allen_shar@email.com"
+                },
+                new Employee{
+                    Id = 4,
+                    Name = "Baichung Bhuchaia",
+                    FirstName = "Baichung",
+                    LastName = "Bhuchaia",
+                    Email = "baichung_bhuchaia@email.com"
+                },
+
+
             };
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> Get()
+        {
+            
+            return Ok(employees);
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<Employee>>> AddEmployee(Employee employee)
+        {
+            employees.Add(employee);
             return Ok(employees);
         }
     }
